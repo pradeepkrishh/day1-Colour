6c2f774513a1472f3278367b35fc5dac5e8db4ec
pradeepnithi <727721euit108@skcet.ac.in>
2023-02-22 12:36:03 +0530

    Third commit

---
+++ src/main/java/com/example/demo/colour.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class colour {
+String str1="White";
+@GetMapping("/game")
+public String getMyFav()
+{
+return "My favourite color is "+str1;
+}
+}
